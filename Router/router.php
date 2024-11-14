6fe9c2dad06f8c999560a7ba233e005bab106885
akmal15051993@gmail.com <akmal15051993@gmail.com>
2024-11-14 19:18:52 +0300

    correct router settings

--- Router/router.php
+++ Router/router.php
@@ -3,6 +3,6 @@
         if($url == "/start") {
             include __DIR__ ."/../html/start.php";
         } else if($url == "/profile") {
-            include __DIR__ ."/../html/profile.php";
+            include __DIR__ ."/../public/html/profile.php";
         }
     }
