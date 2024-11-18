20eeda949c8c4c89897e4c9707fb3371b8a690f9
akmal15051993@gmail.com <akmal15051993@gmail.com>
2024-11-18 16:12:56 +0300

    add initialized composer files

--- Router/router.php
+++ Router/router.php
@@ -3,6 +3,8 @@
         if($url == "/start") {
             include __DIR__ ."/../html/start.php";
         } else if($url == "/profile") {
-            include __DIR__ ."/../public/html/profile.php";
+            include __DIR__ ."/../public/html/profile/profile.php";
+        } else if($url == "/auth") {
+            include __DIR__ ."/../public/html/auth/auth.php";
         } 
     }
