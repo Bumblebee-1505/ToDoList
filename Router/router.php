c5b1f5fe46c443e2159db17feda8483794c111f9
akmal15051993@gmail.com <akmal15051993@gmail.com>
2024-11-18 17:02:21 +0300

    some correct main.php

--- Router/router.php
+++ Router/router.php
@@ -1,8 +1,6 @@
 <?php
     function dispatch($url): void {
-        if($url == "/start") {
-            include __DIR__ ."/../html/start.php";
-        } else if($url == "/profile") {
+        if($url == "/profile") {
             include __DIR__ ."/../public/html/profile/profile.php";
         } else if($url == "/auth") {
             include __DIR__ ."/../public/html/auth/auth.php";
